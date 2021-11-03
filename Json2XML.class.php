543337ec3644446075346f43558e42d2dc18df2e
Samuel Carrara <developer@samuelcarrara.com>
2021-11-03 12:16:32 -0300

    Add root default tag

--- Json2XML.class.php
+++ Json2XML.class.php
@@ -5,24 +5,24 @@
     */
     class Json2XML {
         private function normalize_tag($string){
-            $replace = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
-                                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
-                                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
-                                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
-                                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+            $replace = array("Š"=>"S", "š"=>"s", "Ž"=>"Z", "ž"=>"z", "À"=>"A", "Á"=>"A", "Â"=>"A", "Ã"=>"A", "Ä"=>"A", "Å"=>"A", "Æ"=>"A", "Ç"=>"C", "È"=>"E", "É"=>"E",
+                                "Ê"=>"E", "Ë"=>"E", "Ì"=>"I", "Í"=>"I", "Î"=>"I", "Ï"=>"I", "Ñ"=>"N", "Ò"=>"O", "Ó"=>"O", "Ô"=>"O", "Õ"=>"O", "Ö"=>"O", "Ø"=>"O", "Ù"=>"U",
+                                "Ú"=>"U", "Û"=>"U", "Ü"=>"U", "Ý"=>"Y", "Þ"=>"B", "ß"=>"Ss", "à"=>"a", "á"=>"a", "â"=>"a", "ã"=>"a", "ä"=>"a", "å"=>"a", "æ"=>"a", "ç"=>"c",
+                                "è"=>"e", "é"=>"e", "ê"=>"e", "ë"=>"e", "ì"=>"i", "í"=>"i", "î"=>"i", "ï"=>"i", "ð"=>"o", "ñ"=>"n", "ò"=>"o", "ó"=>"o", "ô"=>"o", "õ"=>"o",
+                                "ö"=>"o", "ø"=>"o", "ù"=>"u", "ú"=>"u", "û"=>"u", "ý"=>"y", "þ"=>"b", "ÿ"=>"y" );
             $string = strtr($string, $replace);
-            $string = preg_replace('/\s+/', ' ', $string);
-            $string = str_replace(' ', '_', $string);
+            $string = preg_replace("/\s+/", " ", $string);
+            $string = str_replace(" ", "_", $string);
             $string = preg_replace("/[^a-zA-Z0-9-_.]/", "", $string);
             return $string;
         }
 
         private function normalize_content($string){
             $string = htmlspecialchars_decode($string, ENT_XML1 | ENT_COMPAT);
-            return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, 'UTF-8');
+            return htmlspecialchars($string, ENT_XML1 | ENT_COMPAT, "UTF-8");
         }
 
-        private function proccess($object, $parent = "root"){
+        private function proccess($object, $parent = ""){
             $xml = "";
             foreach($object as $key => $x){
                 if(is_null($x) || empty($x)){
@@ -78,7 +78,7 @@
             return $xml;
         }
 
-        public function convert($json, $main){
+        public function convert($json, $main = "root"){
             $object = @json_decode($json);
             if(!$object) return false;
             $xml = "";
